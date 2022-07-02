89cce76a91c4c815ec8cf7b01731db1cb8e3c374
KS-Sainen <105944582+KS-Sainen@users.noreply.github.com>
2022-07-02 13:50:58 +0700

    undid the funny

--- CookieIdler.js
+++ CookieIdler.js
@@ -208,7 +208,7 @@ let perkHas = 0;
 
 //Dealing with perks
 let calcCookieToPerk = (level) => {
-    if(!level){
+    if(Number.isNaN(level)){
         return calcCookieToPerk(0);
     }
     return BigP(10,7.5*(level+1));
