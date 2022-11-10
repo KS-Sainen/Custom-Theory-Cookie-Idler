b78e93cd435f88c7321ab3949a3d09759047f8bd
David Jefts <dvdjefts27@gmail.com>
2022-11-10 09:42:59 -0600

    Fix getQuaternaryValues after publish error

--- CookieIdler.js
+++ CookieIdler.js
@@ -2276,7 +2276,6 @@ var get2DGraphValue = () => {
 var getPublicationMultiplier = (tau) => tau.pow(1.078);
 var getPublicationMultiplierFormula = (symbol) => symbol + "^{1.078}";
 var postPublish = () => {
-    quartList = [];
     lump.value = lumpbf;
     hc.value = hbf;
     CPS = BigNumber.ZERO;
